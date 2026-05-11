4c5a196cbd14933d0bfd6f8c2318c26604366594
ah-nd-naf <ahnaf.rasheed.zaki@gmail.com>
2026-05-11 22:37:43 +0600

    added user model

---
+++ src/models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    age: { type: Number },
+    phone: { type: String },
+    isAdmin: { type: Boolean, default: false }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
